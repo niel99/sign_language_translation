e92f3d3a39985151d09b1995fb2cf7d9ef8ba68c
niel99 <ichowdhury883@gmail.com>
2020-04-30 14:48:23 +0530

    Initial commit

---
+++ prototype.py
@@ -0,0 +1,76 @@
+import cv2
+import numpy as np
+import copy
+import math
+
+cap_region_x_begin=0.5  
+cap_region_y_end=0.8  
+threshold = 60  
+blurValue = 41  
+bgSubThreshold = 50
+learningRate = 0
+
+# variables
+isBgCaptured = 0   # bool, whether the background captured
+triggerSwitch = False 
+
+def printThreshold(thr):
+    print("! Changed threshold to "+str(thr))
+
+
+def removeBG(frame):
+    fgmask = bgModel.apply(frame,learningRate=learningRate)
+    kernel = np.ones((3, 3), np.uint8)
+    fgmask = cv2.erode(fgmask, kernel, iterations=1)
+    res = cv2.bitwise_and(frame, frame, mask=fgmask)
+    return res
+camera = cv2.VideoCapture(0)
+camera.set(10,200)
+cv2.namedWindow('trackbar')
+cv2.createTrackbar('trh1', 'trackbar', threshold, 100, printThreshold)
+
+
+while camera.isOpened():
+    ret, frame = camera.read()
+    threshold = cv2.getTrackbarPos('trh1', 'trackbar')
+    frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
+    frame = cv2.flip(frame, 1)  # flip the frame horizontally
+    cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
+                 (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
+    cv2.imshow('original', frame)
+
+    #  Main operation
+    if isBgCaptured == 1:  # this part wont run until background captured
+        img = removeBG(frame)
+        img = img[0:int(cap_region_y_end * frame.shape[0]),
+                    int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
+        cv2.imshow('mask', img)
+
+        # convert the image into binary image
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
+        cv2.imshow('blur', blur)
+        ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY)
+        cv2.imshow('ori', thresh)
+
+
+
+
+    # Keyboard OP
+    k = cv2.waitKey(10)
+    if k == 27:  # press ESC to exit
+        camera.release()
+        cv2.destroyAllWindows()
+        break
+    elif k == ord('b'):  # press 'b' to capture the background
+        bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
+        isBgCaptured = 1
+        print( '!!!Background Captured!!!')
+    elif k == ord('r'):  # press 'r' to reset the background
+        bgModel = None
+        triggerSwitch = False
+        isBgCaptured = 0
+        print ('!!!Reset BackGround!!!')
+    elif k == ord('n'):
+        triggerSwitch = True
+        print ('!!!Trigger On!!!')
